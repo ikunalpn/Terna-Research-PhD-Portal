d8298b74a1013ba646ef56b3c45f7590b1850f0e
Kunal Pandhare <kunalpn7@gmail.com>
2024-03-20 23:31:34 +0530

    rds added

--- PhD_Portal/PhD_Portal/settings.py
+++ PhD_Portal/PhD_Portal/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'PhD_Portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'phd',
+        'USER': 'root',
+        'PASSWORD': 'root1234',
+        'HOST': 'phd.c90m24com6fh.us-east-2.rds.amazonaws.com',   # Or your MySQL server's IP address
+        'PORT': '3306',        # Default MySQL port
     }
 }
+
 
 
 # Password validation
