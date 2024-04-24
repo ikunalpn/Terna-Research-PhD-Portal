1856d0132f28c90d7316c4146203bdc21c13572a
Kunal Pandhare <54733090+ikunalpn@users.noreply.github.com>
2024-04-24 09:40:42 +0530

    Update settings.py

--- PhD_Portal/PhD_Portal/settings.py
+++ PhD_Portal/PhD_Portal/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'PhD_Portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database-1',
-        'USER': 'root',
-        'PASSWORD': 'root1234',
-        'HOST': 'database-1.c90m24com6fh.us-east-2.rds.amazonaws.com',   # Or your MySQL server's IP address
-        'PORT': '3306',        # Default MySQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'database-1',
+#         'USER': 'root',
+#         'PASSWORD': 'root1234',
+#         'HOST': 'database-1.c90m24com6fh.us-east-2.rds.amazonaws.com',   # Or your MySQL server's IP address
+#         'PORT': '3306',        # Default MySQL port
+#     }
+# }
 
 
 
